46e5fa72a546d5b58e28dfcac553165f77bf6107
Marii <mnyrop@users.noreply.github.com>
2017-04-19 15:23:22 -0400

    update name in errors/puts

--- yml_splitter.rb
+++ yml_splitter.rb
@@ -5,20 +5,21 @@ Jekyll::Hooks.register :site, :after_reset do |site| # when site is re/built
 			unless collection.metadata["dir"].nil? || collection.metadata["dir"] == ''
 				targetdir = "_" + collection.metadata["dir"].downcase.gsub(/[^\0-9a-z]/, '').to_s # accepts alphanumeric only, esp. avoiding double _
 			else
-				raise "Yml_Splitter :: Target directory is undefined or unusable. Cannot generate pages. Specify dir in config and rebuild."
+				raise "YAML-Splitter :: Target directory is undefined or unusable. Cannot generate pages. Specify dir in config and rebuild."
 			end
 			unless collection.metadata["layout"].nil? || collection.metadata["layout"] == ''
 				layout = collection.metadata["layout"].to_s
 			else # if no layout is specified, use default
 				layout = "default"
-				puts ">>>> Yml_Splitter :: No layout specified in config. Using default layout."
+				puts ">>>> YAML-Splitter :: No layout specified in config. Using default layout."
 			end
 			FileUtils::mkdir_p targetdir
-			puts ">>>> Yml_Splitter :: Made directory " + targetdir + " in root."
+			puts ">>>> YAML-Splitter :: Made directory " + targetdir + " in root."
 			src = collection.metadata["source"].to_s
 			my_yml = YAML::load(File.open('_data/' + src))
-			puts ">>>> Yml_Splitter :: Loaded " + src + "."
+			puts ">>>> YAML-Splitter :: Loaded " + src + "."
 			page_count = 0
+			skip_count = 0
 			my_yml.each do |item|
 				basename = item["title"].to_s.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '').gsub(/-+/, '-')
 				pagepath = targetdir + "/" + basename + ".md"
@@ -26,11 +27,14 @@ Jekyll::Hooks.register :site, :after_reset do |site| # when site is re/built
 					File.open(pagepath, 'w') { |file| file.write( item.to_yaml.to_s  + "layout: " + layout + "\n---" ) }
 					page_count+=1
 				else
-					puts ">>>> Yml_Splitter :: EEEK! " + basename + ".md already exits."
+					puts ">>>> YAML-Splitter :: EEEK! " + basename + ".md already exits."
+					skip_count+=1
 				end
 			end
-			puts ">>>> Yml_Splitter :: Completed page generation of " + page_count.to_s + " pages from collection data."
+			puts ">>>> YAML-Splitter :: " + page_count.to_s + " pages were generated from " + src + " to new directory " + targetdir + "."
+			puts ">>>> YAML-Splitter :: " + skip_count.to_s + " items were skipped because of non-unique names."
 		end
 	end
 end
+
 
